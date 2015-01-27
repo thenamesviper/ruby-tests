d17284dd09cb4ad631109fca6580fcb8f3516064
James F Odeh <thenamesviper@gmail.com>
2015-01-26 19:05:01 -0800

    rubyspecs

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -5,5 +5,5 @@ def ftoc input
 end
 
 def ctof input
-  (input.to_f)*(9.0/5.0) + 32.0ttrt
+  (input.to_f)*(9.0/5.0) + 32.0
  end
--- 02_calculator/calculator_spec.rb
+++ 02_calculator/calculator_spec.rb
@@ -79,21 +79,43 @@ end
 
 describe "#multiply" do
 
-  it "multiplies two numbers"
+  it "multiplies two numbers" do
+	multiply([4,7]).should == 28
+	end
 
-  it "multiplies several numbers"
-
+  it "multiplies several numbers" do
+	multiply([4,5,7]).should == 140
+	end
 end
 
 describe "#power" do
-  it "raises one number to the power of another number"
+  it "raises one number to the power of another number" do
+	power(3,3).should == 27
+  end
+  
+  it "raises two to the zero" do
+	power(2,0).should ==1
+	end
 end
 
 # http://en.wikipedia.org/wiki/Factorial
 describe "#factorial" do
-  it "computes the factorial of 0"
-  it "computes the factorial of 1"
-  it "computes the factorial of 2"
-  it "computes the factorial of 5"
-  it "computes the factorial of 10"
+  it "computes the factorial of 0" do
+	factorial(0).should == 1	 #0! is 1, not 0
+  end
+  
+  it "computes the factorial of 1" do
+	factorial(1).should == 1
+  end
+  
+  it "computes the factorial of 2" do
+	factorial(2).should == 2
+  end
+  
+  it "computes the factorial of 5" do
+	factorial(5).should == 120
+  end
+  it "computes the factorial of 10" do
+	factorial(10).should == 3628800
+  end
 end
